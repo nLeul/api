8e4eb88d8114819818d61381cfc6a7c31f352319
Leul Necha <49601331+nLeul@users.noreply.github.com>
2020-06-07 16:13:16 -0500

    Add files via upload
    api

---
+++ App.js
@@ -0,0 +1,72 @@
+const express = require('./node_modules/express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config()
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+
+const MONGODB_URL=`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PWD}@restapicluster0-1pve1.mongodb.net/${process.env.MONGO_DBName}?retryWrites=true&w=majority`
+
+const CourseSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    code: {
+        type: String,
+        required: true
+    },
+    topics: {
+        type: [],
+        required: true
+    }
+})
+const myCourse = mongoose.model('Course', CourseSchema);
+
+
+
+app.get('/course', (req, res) => {
+    myCourse.find()
+        .then(course => res.json({ success: true, data: course }))
+        .catch(err => res.json({ success: false, err }))
+});
+app.post('/addCourse', (req, res) => {
+    myCourse.create(req.body)
+        .then(course => res.json({ success: true, data: course }))
+        .catch(err => res.json({ success: false, err }))
+});
+
+
+app.delete('/delete', (req, res) => {
+    const id = req.query.id;
+    myCourse.findByIdAndDelete({ _id: id })
+        .then(course => res.json({ success: true, data: "course succesfully deleted" }))
+        .catch(err => res.json({ success: false, err }))
+});
+
+app.put('/update', (req, res) => {
+    const id = req.query.id;
+    myCourse.findByIdAndUpdate({ _id: id }, req.body)
+        .then(course => res.json({ success: true, data: "course succesfully updated" }))
+        .catch(err => res.json({ success: false, err }))
+});
+
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id;
+    // console.log(id);
+    myCourse.findById({ _id: id })
+        .then(course => res.json({ success: true, data: course }))
+        .catch(err => res.json({ success: false, err }))
+});
+// console.log(process.env.PORT)
+
+mongoose.connect(MONGODB_URL, { useUnifiedTopology: true, useNewUrlParser: true })
+    .then(() => {
+        app.listen(3000, () => console.log(`App running`));
+    })
+    .catch(err => console.log(err))
