8cb91660790ec332ffa681665ecaf47165fce867
nLeul <leulnecha@gmail.com>
2020-06-08 02:01:17 -0500

    deployment final

--- App.js
+++ App.js
@@ -1,13 +1,26 @@
 const express = require('./node_modules/express');
+const aws = require('aws-sdk');//to set  deployment -env variables for heroku
 const bodyParser = require('body-parser');
 const cors = require('cors');
-require('dotenv').config()
+// require('dotenv').config()
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
+
+
+
+
+let s3 = new aws.S3({
+    MONGO_USER: process.env.MONGO_USER,
+    MONGO_PWD: process.env.MONGO_PWD,
+    MONGO_DBName: process.env.MONGO_DBName,
+    PORT: process.env.PORT,
+
+});
+
 
 
 const MONGODB_URL=`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PWD}@restapicluster0-1pve1.mongodb.net/${process.env.MONGO_DBName}?retryWrites=true&w=majority`
@@ -67,6 +80,6 @@ app.get('/course/:id', (req, res) => {
 
 mongoose.connect(MONGODB_URL, { useUnifiedTopology: true, useNewUrlParser: true })
     .then(() => {
-        app.listen(3000, () => console.log(`App running`));
+        app.listen(`${process.env.PORT}`, () => console.log(`App running`));
     })
     .catch(err => console.log(err))
